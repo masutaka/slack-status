8f6b1e5c5e7600df816c329f0f1165b056c0c245
Takashi Masuda <masutaka.net@gmail.com>
2020-07-07 12:28:12 +0900

    Set DND when finish

--- slack.rb
+++ slack.rb
@@ -37,8 +37,9 @@ if JOBCAN_CHANNEL_ID.nil?
   exit 1
 end
 
-SLACK_API_ROOT="#{slack_url}/api"
+SLACK_API_ROOT = "#{slack_url}/api"
 SET_PROFILE_URL = "#{SLACK_API_ROOT}/users.profile.set?token=#{token}"
+SET_DND_URL = "#{SLACK_API_ROOT}/dnd.setSnooze?token=#{token}"
 
 # Undocumented API
 # See https://github.com/slack-ruby/slack-api-ref/blob/master/methods/undocumented/chat/chat.command.json
@@ -50,10 +51,20 @@ end
 
 # Get slack status_expiration
 #
+# @param now [Time]
 # @param minutes [Number] 0 means clear
-def slack_status_expiration(minutes)
+def slack_status_expiration(now, minutes)
   m = minutes.to_i
-  m == 0 ? 0 : (Time.now + m*60).to_i
+  m == 0 ? 0 : (now + m*60).to_i
+end
+
+# Set DND until AM9:00 on tomorrow
+#
+# @param now [Time]
+def slack_set_dnd(now)
+  target = (now.to_date + 1).to_time + 9*60*60 # AM9:00 on tomorrow
+  num_minutes = ((target - now) / 60).to_i     # sub minutes
+  Net::HTTP.post_form(URI.parse(SET_DND_URL), num_minutes: num_minutes)
 end
 
 def jobcan_touch
@@ -65,26 +76,29 @@ def jobcan_touch
   )
 end
 
+now = Time.now
+
 case ARGV[0]
 when 'start'
   slack_set_status(
     status_emoji: SLACK_START_EMOJI,
     status_text: SLACK_START_TEXT,
-    status_expiration: slack_status_expiration(SLACK_START_EXPIRE_MINUTES),
+    status_expiration: slack_status_expiration(now, SLACK_START_EXPIRE_MINUTES),
   )
   jobcan_touch
 when 'lunch'
   slack_set_status(
     status_emoji: SLACK_LUNCH_EMOJI,
     status_text: SLACK_LUNCH_TEXT,
-    status_expiration: slack_status_expiration(SLACK_LUNCH_EXPIRE_MINUTES),
+    status_expiration: slack_status_expiration(now, SLACK_LUNCH_EXPIRE_MINUTES),
   )
 when 'finish'
   slack_set_status(
     status_emoji: SLACK_FINISH_EMOJI,
     status_text: SLACK_FINISH_TEXT,
-    status_expiration: slack_status_expiration(SLACK_FINISH_EXPIRE_MINUTES),
+    status_expiration: slack_status_expiration(now, SLACK_FINISH_EXPIRE_MINUTES),
   )
+  slack_set_dnd(now)
   jobcan_touch
 else
   puts 'Usage: slack.rb { start | lunch | finish }'
